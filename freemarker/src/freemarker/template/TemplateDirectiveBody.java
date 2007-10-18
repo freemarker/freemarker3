d5a824c91b8f7a12f85688353f602b4561e622fd
Attila Szegedi <szegedi@users.noreply.github.com>
2007-10-18 10:46:29 +0000

    - Disallow null as writer in body - pass Environment instead of Writer to directive model

--- freemarker/src/freemarker/template/TemplateDirectiveBody.java
+++ freemarker/src/freemarker/template/TemplateDirectiveBody.java
@@ -5,9 +5,10 @@ import java.io.Writer;
 
 /**
  * Represents the body of a directive invocation. An implementation of this 
- * class is passed to the {@link TemplateDirectiveModel#execute(java.io.Writer, 
- * java.util.Map, TemplateDirectiveBody)}. The implementation of the method is 
- * free to invoke it any number of times, with any writer.
+ * class is passed to the {@link TemplateDirectiveModel#execute(
+ * freemarker.core.Environment, java.util.Map, TemplateDirectiveBody)}. The 
+ * implementation of the method is free to invoke it any number of times, with 
+ * any writer.
  * @author Attila Szegedi
  * @version $Id: $
  */
@@ -15,8 +16,7 @@ public interface TemplateDirectiveBody
 {
     /**
      * Renders the body of the directive body to the specified writer.
-     * @param out the writer to write the output to. If null, the environment's
-     * writer is used.
+     * @param out the writer to write the output to.
      */
     public void render(Writer out) throws TemplateException, IOException;
 }
