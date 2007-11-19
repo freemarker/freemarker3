a6603ed24e07b6e9c6fcb4798ff05654bc78b389
ddekany <ddekany@320ef0a2-c40d-0410-b545-cffc488e3532>
2007-11-19 04:47:59 +0000

    - Improving TemplateDirectiveModel JavaDocs; please use the "official" terminology in the JavaDocs (or propose a terminology change).
    - Updating TemplateDirectiveModel documentation in the Manual

--- trunk/freemarker/src/freemarker/template/TemplateDirectiveModel.java
+++ trunk/freemarker/src/freemarker/template/TemplateDirectiveModel.java
@@ -7,31 +7,39 @@ import freemarker.core.Environment;
 
 /**
  * Objects that implement this interface can be used as user-defined directives 
- * (much like macros). They can implement arbitrary code, write arbitrary
+ * (much like macros). They can do arbitrary actions, write arbitrary
  * text to the template output, and trigger rendering of their nested content
  * any number of times.
  *
+ * @since 2.3.11
  * @author Attila Szegedi
  * @version $Id: $
  */
 public interface TemplateDirectiveModel extends TemplateModel
 {
     /**
-     * Executes this directive once.
-     * @param env the current processing environment
-     * @param args the arguments (if any) passed to the transformation as a 
-     * map of key/value pairs where the keys are strings and the arguments are
-     * TemplateModel instances. This is never null.
-     * @param outArgs an array containing values for out arguments (also called
-     * "loop variables"), in order declared in the invocation, available to 
-     * the nested body of the directive. Never null, but can be a zero-length array.
-     * @param body an object that can be used to render the body of the 
-     * user-defined directive call. If the user-defined directive is 
-     * constructed with the empty tag syntax, this will be null.
+     * Executes this user-defined directive; called by FreeMarker when the user-defined
+     * directive is called in the template.
+     *
+     * @param env the current processing environment. Note that you can access
+     * the output {@link java.io.Writer Writer} by {@link Environment#getOut()}.
+     * @param params the parameters (if any) passed to the directive as a 
+     * map of key/value pairs where the keys are {@link String}-s and the values are
+     * {@link TemplateModel} instances. This is never <code>null</code>.
+     * @param loopVars an array that corresponds to the "loop variables", in
+     * the order as they appear in the directive call. ("Loop variables" are out-parameters
+     * that are available to the nested body of the directive; see in the Manual.)
+     * You set the loop variables by writing this array. The length of the array gives the
+     * number of loop-variables that the caller has specified.
+     * Never <code>null</code>, but can be a zero-length array.
+     * @param body an object that can be used to render the nested content (body) of
+     * the directive call. If the directive call has no nested content (i.e., it is
+     * [@myDirective /] or [@myDirective][/@myDirective]), then this will be
+     * <code>null</code>.
+     *
      * @throws TemplateException
      * @throws IOException
      */
-    public void execute(Environment env, Map<String, TemplateModel> args, 
-            TemplateModel[] outArgs, TemplateDirectiveBody body) 
-    throws TemplateException, IOException;
+    public void execute(Environment env, Map params, TemplateModel[] loopVars, 
+            TemplateDirectiveBody body) throws TemplateException, IOException;
 }
