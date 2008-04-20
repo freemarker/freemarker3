71c0ef78a7ecc894b75520554e3226905cc146b4
szegedia <szegedia@320ef0a2-c40d-0410-b545-cffc488e3532>
2008-04-20 14:48:41 +0000

    Unwrap Rhino wrappers and re-wrap what's in them (instead of wrapping the wrapper)

--- trunk/freemarker/src/freemarker/ext/rhino/RhinoWrapper.java
+++ trunk/freemarker/src/freemarker/ext/rhino/RhinoWrapper.java
@@ -1,9 +1,9 @@
 package freemarker.ext.rhino;
 
-import org.mozilla.javascript.Function;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.Undefined;
 import org.mozilla.javascript.UniqueTag;
+import org.mozilla.javascript.Wrapper;
 
 import freemarker.ext.beans.BeansWrapper;
 import freemarker.ext.util.ModelFactory;
@@ -30,6 +30,10 @@ public class RhinoWrapper extends BeansWrapper {
         // it, so we'll just be on the safe side, and handle it.
         if(obj == UniqueTag.NULL_VALUE) {
             return TemplateModel.JAVA_NULL;
+        }
+        // So, say, a JavaAdapter for FreeMarker interfaces works
+        if(obj instanceof Wrapper) {
+            obj = ((Wrapper)obj).unwrap();
         }
         return super.wrap(obj);
     }
