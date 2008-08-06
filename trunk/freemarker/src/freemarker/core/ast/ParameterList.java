0455cad3b3ff512b5ab0730bc050b23619b735b1
szegedia <szegedia@320ef0a2-c40d-0410-b545-cffc488e3532>
2008-08-06 21:31:12 +0000

    Enhancements to invocations: - defaults defined through other defaults work (in all scenarios, i.e. even for TDM/TMM/TTM with Parameters annotation) - defaults are applied when TDM/TMM/TTM is invoked with no args, but has defaults for all args (handled by EmptyArgsList)

--- trunk/freemarker/src/freemarker/core/ast/ParameterList.java
+++ trunk/freemarker/src/freemarker/core/ast/ParameterList.java
@@ -3,7 +3,6 @@ package freemarker.core.ast;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
@@ -15,6 +14,7 @@ import freemarker.core.InvalidReferenceException;
 import freemarker.core.Scope;
 import freemarker.core.TemplateRunnable;
 import freemarker.core.helpers.NamedParameterListScope;
+import freemarker.core.helpers.NamedParameterMapScope;
 import freemarker.template.SimpleHash;
 import freemarker.template.SimpleSequence;
 import freemarker.template.TemplateException;
@@ -23,8 +23,10 @@ import freemarker.template.TemplateModelException;
 
 /**
  * Represents the specification of the parameters to a macro.
- * Note that we deal with default values and the optional catch-al
+ * Note that we deal with default values and the optional catch-all
  * parameter here (varags...)
+ * @version $Id: $
+ * @since 2.4
  */
 
 //TODO: Improve the error detail messages maybe
@@ -74,16 +76,30 @@ public class ParameterList extends TemplateNode {
         return curryGenerated;
     }
 
+    public boolean hasDefaultExpressions() {
+        return !(defaults == null || defaults.isEmpty());
+    }
+    
     public Expression getDefaultExpression(String paramName) {
         return defaults == null ? null : defaults.get(paramName);
     }
 
-    @Deprecated
-    public void fillInDefaults(Scope scope) throws TemplateException {
-        fillInDefaults(scope, params);
+    private void fillInDefaults(final Environment env, final Scope scope, final Collection<String> paramNames) 
+    throws TemplateException {
+        try {
+            env.runInScope(scope, new TemplateRunnable<Object>() {
+                public Object run() throws TemplateException, IOException {
+                    fillInDefaultsInternal(env, scope, paramNames);
+                    return null;
+                }
+            });
+        }
+        catch(IOException e) {
+            throw new TemplateException(e, env);
+        }
     }
-    
-    private void fillInDefaults(Scope scope, Collection<String> paramNames) throws TemplateException {
+    private void fillInDefaultsInternal(Environment env, Scope scope, Collection<String> paramNames) throws TemplateException {
+        
         boolean resolvedAnArg, hasUnresolvedArg;
         Expression firstUnresolvedExpression;
         InvalidReferenceException firstReferenceException;
@@ -97,7 +113,7 @@ public class ParameterList extends TemplateNode {
                     Expression defaultExp = getDefaultExpression(paramName);
                     if (defaultExp != null) {
                         try {
-                            TemplateModel value = defaultExp.getAsTemplateModel(scope.getEnvironment());
+                            TemplateModel value = defaultExp.getAsTemplateModel(env);
                             if(value == null || value == TemplateModel.JAVA_NULL) {
                                 if(!hasUnresolvedArg) {
                                     firstUnresolvedExpression = defaultExp;
@@ -134,40 +150,15 @@ public class ParameterList extends TemplateNode {
         }
     }
 
+    /**
+     * Given a positional list of argument expressions, create a positional 
+     * list of template models. Used to pass positional arguments to a template
+     * method model.
+     */
     List<TemplateModel> getParameterSequence(final PositionalArgsList args, 
             final Environment env) 
     throws TemplateException {
         final List<TemplateModel> result = new ArrayList<TemplateModel>(params.size());
-        if(defaults == null || defaults.isEmpty() || args.size() >= params.size()) {
-            // There'll be no need to fill in defaults, so avoid creating a new
-            // temporary scope
-            getParameterSequence(result, args, env);
-        }
-        else {
-            // Create a scope that provides live access to the parameter list
-            // so we can reference already defined parameters
-            try {
-                final Scope scope = new NamedParameterListScope(
-                        env.getCurrentScope(), params, result, false);
-                env.runInScope(scope, new TemplateRunnable<Object>() {
-                        public Object run() throws TemplateException {
-                            getParameterSequence(result, args, env);
-                            fillInDefaults(scope, params.subList(args.size(), 
-                                    params.size()));
-                            return null;
-                        }
-                    });
-            }
-            catch(IOException e) {
-                throw new TemplateException(e, env);
-            }
-        }
-        return result;
-    }
-    
-    private void getParameterSequence(List<TemplateModel> result, 
-            PositionalArgsList args, Environment env)
-    throws TemplateException {
         int argsSize = args.size();
         int paramsSize = params.size();
         int commonSize = Math.min(argsSize, paramsSize);
@@ -185,8 +176,23 @@ public class ParameterList extends TemplateNode {
                 result.add(args.getValueAt(i, env));
             }
         }
-    }
+        else if(commonSize < paramsSize) {
+            // More formal args than actual args -- fill in defaults
 
+            // Create a scope that provides live access to the parameter list
+            // so we can reference already defined parameters
+            Scope scope = new NamedParameterListScope(env.getCurrentScope(), 
+                    params, result, false);
+                fillInDefaults(env, scope, params.subList(args.size(), params.size()));
+        }
+        return result;
+    }
+    
+    /**
+     * Given a named list of argument expressions, create a positional 
+     * list of template models. Used to pass named arguments to a template
+     * method model.
+     */
     List<TemplateModel> getParameterSequence(final NamedArgsList args, 
             final Environment env) 
     throws TemplateException {
@@ -198,35 +204,6 @@ public class ParameterList extends TemplateNode {
             throw new TemplateException("Extraneous parameters " + l, env);
         }
         final List<TemplateModel> result = new ArrayList<TemplateModel>();
-        if(defaults == null || defaults.isEmpty() || argsSize == paramsSize) {
-            // There'll be no need to fill in defaults, so avoid creating a new
-            // temporary scope
-            getParameterSequence(result, args, env);
-        }
-        else {
-            // Create a scope that provides live access to the parameter list
-            // so we can reference already defined parameters
-            try {
-                final Scope scope = new NamedParameterListScope(
-                        env.getCurrentScope(), params, result, false);
-                env.runInScope(scope, new TemplateRunnable<Object>() {
-                        public Object run() throws TemplateException {
-                            fillInDefaults(scope, getParameterSequence(result, 
-                                    args, env));
-                            return null;
-                        }
-                    });
-            }
-            catch(IOException e) {
-                throw new TemplateException(e, env);
-            }
-        }
-        return result;
-    }
-
-    private List<String> getParameterSequence(List<TemplateModel> result,
-            NamedArgsList args, Environment env) throws TemplateException
-    {
         List<String> unresolvedParamNames = null;
         Map<String, Expression> argsMap = args.getCopyOfMap();
         for (String paramName : params) {
@@ -242,58 +219,57 @@ public class ParameterList extends TemplateNode {
                 unresolvedParamNames.add(paramName);
             }
         }
-        if(unresolvedParamNames == null) {
-            unresolvedParamNames = Collections.emptyList();
+        if(unresolvedParamNames != null) {
+            // Create a scope that provides live access to the parameter list
+            // so we can reference already defined parameters
+            final Scope scope = new NamedParameterListScope(
+                    env.getCurrentScope(), params, result, false);
+            fillInDefaults(env, scope, unresolvedParamNames);
         }
-        return unresolvedParamNames;
+        return result;
     }
 
     /**
      * Given a positional args list, creates a map of key-value pairs based
      * on the named parameter info encapsulated in this object. 
      */
-    public Map<String, TemplateModel> getParameterMap(PositionalArgsList args, Environment env, boolean ignoreExtraParams) 
+    public Map<String, TemplateModel> getParameterMap(final PositionalArgsList args, 
+            final Environment env, boolean ignoreExtraParams) 
     throws TemplateException 
     {
-        Map<String, TemplateModel> result = new HashMap<String, TemplateModel>();
-        SimpleSequence catchAllVars = null;
-        if (catchall != null) {
-            catchAllVars = new SimpleSequence();
+        final int argsSize = args.size();
+        final int paramsSize = params.size();
+        final Map<String, TemplateModel> result = new HashMap<String, TemplateModel>();
+        if (catchall == null && argsSize > paramsSize && !ignoreExtraParams) {
+            throw new TemplateException("Expecting exactly " + paramsSize + 
+                    " arguments, received " + argsSize + ".", env);
+        }
+        int min = Math.min(paramsSize, argsSize);
+        for (int i=0; i < min; i++) {
+            result.put(params.get(i), args.getValueAt(i, env));
+        }
+        if(defaults != null && !defaults.isEmpty() && argsSize < paramsSize) {
+            // Create a scope that provides live access to the parameter list
+            // so we can reference already defined parameters
+            Scope scope = new NamedParameterMapScope(env.getCurrentScope(), 
+                    result);
+            fillInDefaults(env, scope, params.subList(argsSize, paramsSize));
+        }
+        if(catchall != null) {
+            SimpleSequence catchAllVars = new SimpleSequence();
             result.put(catchall, catchAllVars);
-        }
-        if (args.size() > params.size()) {
-            if (this.catchall == null) {
-                // TODO location info
-                if (!ignoreExtraParams)
-                    throw new TemplateException("Expecting exactly " + params.size() + " arguments, received " + args.size() + " parameters.", env);
-            } else {
-                for (int i=params.size(); i<args.size(); i++) {
-                    catchAllVars.add(args.getValueAt(i, env));
-                }
-            }
-        }
-        for (int i=0; i < params.size(); i++) {
-            String paramName = params.get(i);
-            if (i<args.size()) {
-                result.put(paramName, args.getValueAt(i, env));
-            } 
-            else {
-                Expression defaultExp = defaults.get(paramName);
-                if (defaultExp == null) {
-                    throw new TemplateException("Parameter " + paramName + " unspecified.", env); 
-                }
-                TemplateModel defaultValue = defaultExp.getAsTemplateModel(env);
-                assertIsDefined(defaultValue, defaultExp, env);
-                result.put(paramName, defaultValue);
+            for (int i = paramsSize; i < argsSize; i++) {
+                catchAllVars.add(args.getValueAt(i, env));
             }
         }
         return result;
     }
-
+    
     public Map<String, TemplateModel> getParameterMap(NamedArgsList args, Environment env) 
     throws TemplateException 
     {
         Map<String, TemplateModel> result = new HashMap<String, TemplateModel>();
+        Collection<String> unresolvedParamNames = null;
         Map<String, Expression> argsMap = args.getCopyOfMap();
         for (String paramName : params) {
             Expression argExp = argsMap.remove(paramName);
@@ -302,22 +278,27 @@ public class ParameterList extends TemplateNode {
                 TemplateNode.assertIsDefined(value, argExp, env);
                 result.put(paramName, value);
             }
-            else {
-                Expression defaultExp = defaults.get(paramName);
-                if (defaultExp == null) {
-                    //TODO location info.
-                    throw new TemplateException("Missing required parameter " + paramName, env);
+            else if(defaults != null && defaults.containsKey(paramName)) {
+                if(unresolvedParamNames == null) {
+                    unresolvedParamNames = new LinkedList<String>();
                 }
-                TemplateModel value = defaultExp.getAsTemplateModel(env);
-                //assertIsDefined(value, defaultExp, env);
-                result.put(paramName, value);
+                unresolvedParamNames.add(paramName);
             }
+            else {
+                throw new TemplateException("Missing required parameter " + paramName, env);
+            }
+        }
+        if(unresolvedParamNames != null) {
+            // Create a scope that provides live access to the parameter list
+            // so we can reference already defined parameters
+            Scope scope = new NamedParameterMapScope(env.getCurrentScope(), 
+                    result);
+            fillInDefaults(env, scope, unresolvedParamNames);
         }
         SimpleHash catchAllMap = null;
         if (catchall != null) {
             catchAllMap = new SimpleHash();
             result.put(catchall, catchAllMap);
-
         }
         if (!argsMap.isEmpty()) {
             if(catchall != null || curryGenerated) {
@@ -339,11 +320,31 @@ public class ParameterList extends TemplateNode {
         return result;
     }
 
+    public Map<String, TemplateModel> getParameterMapForEmptyArgs(Environment env) 
+    throws TemplateException 
+    {
+        Map<String, TemplateModel> result = new HashMap<String, TemplateModel>();
+        if(hasDefaultExpressions()) {
+            // Create a scope that provides live access to the parameter list
+            // so we can reference already defined parameters
+            Scope scope = new NamedParameterMapScope(env.getCurrentScope(), 
+                    result);
+            fillInDefaults(env, scope, defaults.keySet());
+        }
+        return result;
+    }
+
     public Map<String, TemplateModel> getParameterMap(ArgsList args, Environment env) throws TemplateException {
         if (args instanceof NamedArgsList) {
             return getParameterMap((NamedArgsList) args, env);
+        } 
+        if(args instanceof PositionalArgsList) {
+            return getParameterMap((PositionalArgsList) args, env, false);
         }
-        return getParameterMap((PositionalArgsList) args, env, false);
+        if (args instanceof EmptyArgsList) {
+            return getParameterMapForEmptyArgs(env);
+        } 
+        throw new AssertionError();
     }
 
     String toDebugString() {
