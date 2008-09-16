f708d0a55c43b8a0f95d6769646865dc5bc95d34
Attila Szegedi <szegedi@users.noreply.github.com>
2008-09-16 13:15:24 +0000

    Use a single precreated Break instance, to avoid costly (and unneeded) fillInStackTrace in exception constructor

--- freemarker/src/freemarker/core/BreakException.java
+++ freemarker/src/freemarker/core/BreakException.java
@@ -55,7 +55,7 @@ package freemarker.core;
 /**
  * An exception used to break out of a loop
  */
-
 public class BreakException extends RuntimeException {
     private static final long serialVersionUID = -3716099923723830891L;
+    public static BreakException INSTANCE = new BreakException();
 }
