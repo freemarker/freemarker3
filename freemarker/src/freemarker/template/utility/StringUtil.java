dcf9ff873c2222d39c43a60bec97ac51179dbd0c
ddekany <ddekany@freemail.hu>
2009-09-11 10:09:23 +0000

    Fixed XML-escaping bug where XMLEncNQG didn't escape "]]>" as "]]&gt;", which is requested by the XML spec. (Other occurrences of > still need not be escaped.)

--- freemarker/src/freemarker/template/utility/StringUtil.java
+++ freemarker/src/freemarker/template/utility/StringUtil.java
@@ -220,41 +220,57 @@ public class StringUtil {
     }
 
     /**
-     *  XML encoding without replacing apostrophes and quotation marks and greater-than signs.
+     *  XML encoding without replacing apostrophes and quotation marks and
+     *  greater-thans (except in {@code ]]>}).
      *  @see #XMLEnc(String)
      */
     public static String XMLEncNQG(String s) {
         int ln = s.length();
         for (int i = 0; i < ln; i++) {
             char c = s.charAt(i);
-            if (c == '<' || c == '&') {
-                StringBuilder b =
-                        new StringBuilder(s.substring(0, i));
+            if (c == '<'
+                    || (c == '>' && i > 1
+                            && s.charAt(i - 1) == ']'
+                            && s.charAt(i - 2) == ']')
+                    || c == '&') {
+                StringBuffer b =
+                        new StringBuffer(s.substring(0, i));
                 switch (c) {
                     case '<': b.append("&lt;"); break;
+                    case '>': b.append("&gt;"); break;
                     case '&': b.append("&amp;"); break;
+                    default: throw new RuntimeException("Bug: unexpected char");
                 }
                 i++;
                 int next = i;
                 while (i < ln) {
                     c = s.charAt(i);
-                    if (c == '<' || c == '&') {
+                    if (c == '<'
+                            || (c == '>' && i > 1
+                                    && s.charAt(i - 1) == ']'
+                                    && s.charAt(i - 2) == ']')
+                            || c == '&') {
                         b.append(s.substring(next, i));
                         switch (c) {
                             case '<': b.append("&lt;"); break;
+                            case '>': b.append("&gt;"); break;
                             case '&': b.append("&amp;"); break;
+                            default: throw new RuntimeException("Bug: unexpected char");
                         }
                         next = i + 1;
                     }
                     i++;
                 }
-                if (next < ln) b.append(s.substring(next));
+                if (next < ln) {
+                    b.append(s.substring(next));
+                }
                 s = b.toString();
                 break;
             } // if c ==
         } // for
         return s;
     }
+    
     /**
      *  Rich Text Format encoding (does not replace line breaks).
      *  Escapes all '\' '{' '}' and '"'
