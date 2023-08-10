4a6179798f0d486bc61d69e28419f1b73d4479be
revusky <revusky@gmail.com>
2023-08-11 05:49:15 +0900

    A bit more incremental work.

--- src/freemarker/core/ast/Dot.java
+++ src/freemarker/core/ast/Dot.java
@@ -1,6 +1,7 @@
 package freemarker.core.ast;
 
 import freemarker.core.Environment;
+import freemarker.core.parser.Token;
 import freemarker.core.parser.ast.Identifier;
 import freemarker.template.TemplateHashModel;
 
@@ -9,36 +10,38 @@ import freemarker.template.TemplateHashModel;
  * <code>TemplateHashModel</code>.
  */
 public class Dot extends Expression {
-    private Expression target;
-    private String key;
+//    private Expression target;
+//    private String key;
 
-    public Dot(Expression target, String key) {
-        this.target = target;
-        target.setParent(this);
-        this.key = key;
+    public Dot() {}
+
+    public Dot(Expression target, Token key) {
+        add(target);
+        add(key);
     }
     
     public Expression getTarget() {
-    	return target;
+    	return (Expression) get(0);
     }
     
     public String getKey() {
-    	return key;
+    	return get(1).toString();
     }
 
     public Object getAsTemplateModel(Environment env) {
-        Object leftModel = target.getAsTemplateModel(env);
+        Object leftModel = getTarget().getAsTemplateModel(env);
         if(leftModel instanceof TemplateHashModel) {
-            return ((TemplateHashModel) leftModel).get(key);
+            return ((TemplateHashModel) leftModel).get(getKey());
         }
-        throw invalidTypeException(leftModel, target, env, "hash");
+        throw invalidTypeException(leftModel, getTarget(), env, "hash");
     }
 
     public Expression _deepClone(String name, Expression subst) {
-    	return new Dot(target.deepClone(name, subst), key);
+    	return new Dot(getTarget().deepClone(name, subst), (Token) get(1));
     }
 
     public boolean onlyHasIdentifiers() {
+        Expression target = getTarget();
         return (target instanceof Identifier) 
                || ((target instanceof Dot) 
                && ((Dot) target).onlyHasIdentifiers());
