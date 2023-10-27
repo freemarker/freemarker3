4cce5ff1167eee7a35dc0c1baf0873614928ae53
revusky <revusky@gmail.com>
2023-10-27 20:34:23 +0200

    Breaking AdditiveExpression.java into a hand-maintained file for now

---
+++ src/freemarker/core/nodes/AdditiveExpression.java
@@ -0,0 +1,185 @@
+package freemarker.core.nodes;
+
+import freemarker.core.EvaluationUtil;
+import freemarker.core.ArithmeticEngine;
+import freemarker.core.Environment;
+import freemarker.core.nodes.generated.Expression;
+import freemarker.core.nodes.generated.TemplateNode;
+import freemarker.template.*;
+import java.util.*;
+
+import static freemarker.core.parser.Token.TokenType.*;
+import static freemarker.ext.beans.ObjectWrapper.*;
+
+
+//THIS NEEDS TO BE SIMPLIFIED!
+public class AdditiveExpression extends TemplateNode implements Expression {
+
+    public boolean isPlus() {
+        return get(1).getType() == PLUS;
+    }
+
+    public Expression getLeft() {
+        return (Expression) get(0);
+    }
+
+    public Expression getRight() {
+        return (Expression) get(2);
+    }
+
+    public Object evaluate(Environment env) {
+        Object left = getLeft().evaluate(env);
+        Object right = getRight().evaluate(env);
+        ArithmeticEngine ae = env != null ? env.getArithmeticEngine() : getTemplate().getArithmeticEngine();
+        if (!isPlus()) {
+            Number first = EvaluationUtil.getNumber(left, getLeft(), env);
+            Number second = EvaluationUtil.getNumber(right, getRight(), env);
+            return ae.subtract(first, second);
+        }
+        boolean bothAreNumbers = isNumber(left) && isNumber(right);
+        if (bothAreNumbers) {
+            Number first = EvaluationUtil.getNumber(left, getLeft(), env);
+            Number second = EvaluationUtil.getNumber(right, getRight(), env);
+            return ae.add(first, second);
+        }
+        if (isList(left) && isList(right)) {
+            /*List merged = new ArrayList(asList(leftModel));
+            merged.addAll(asList(rightModel));
+            return merged;*/
+            return new ConcatenatedSequence((WrappedSequence) left, (WrappedSequence) right);
+        }
+        if (isDisplayableAsString(left) && isDisplayableAsString(right)) {
+            return asString(left) + asString(right);
+        }
+        if (left instanceof WrappedHash && right instanceof WrappedHash) {
+            if (left instanceof WrappedHash && right instanceof WrappedHash) {
+                WrappedHash leftModelEx = (WrappedHash) left;
+                WrappedHash rightModelEx = (WrappedHash) right;
+                if (leftModelEx.size() == 0) {
+                    return rightModelEx;
+                } else if (rightModelEx.size() == 0) {
+                    return leftModelEx;
+                } else {
+                    return new ConcatenatedHashEx(leftModelEx, rightModelEx);
+                }
+            } else {
+                return new ConcatenatedHash((WrappedHash) left, (WrappedHash) right);
+            }
+        }
+        String msg = this.getLocation() + ": Cannot add or concatenate";
+        throw new TemplateException(msg, env);
+    }
+
+    public Expression _deepClone(String name, Expression subst) {
+        AdditiveExpression result = new AdditiveExpression();
+        result.add(getLeft().deepClone(name, subst));
+        result.add(get(1));
+        result.add(getRight().deepClone(name, subst));
+        return result;
+    }
+
+
+    private static final class ConcatenatedSequence implements WrappedSequence {
+        private final WrappedSequence left;
+        private final WrappedSequence right;
+
+        ConcatenatedSequence(WrappedSequence left, WrappedSequence right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public int size() {
+            return left.size() + right.size();
+        }
+
+        public Object get(int i) {
+            int ls = left.size();
+            return i < ls ? left.get(i) : right.get(i - ls);
+        }
+
+    }
+
+
+    private static class ConcatenatedHash implements WrappedHash {
+        final WrappedHash left;
+        final WrappedHash right;
+
+        ConcatenatedHash(WrappedHash left, WrappedHash right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public Object get(String key) {
+            Object model = right.get(key);
+            return (model != null) ? model : left.get(key);
+        }
+
+        public boolean isEmpty() {
+            return left.isEmpty() && right.isEmpty();
+        }
+
+    }
+
+
+    private static final class ConcatenatedHashEx extends ConcatenatedHash implements WrappedHash {
+        private Iterable<String> keys;
+        private Iterable<Object> values;
+        private int size;
+
+        ConcatenatedHashEx(WrappedHash left, WrappedHash right) {
+            super(left, right);
+        }
+
+        public int size() throws EvaluationException {
+            initKeys();
+            return size;
+        }
+
+        public Iterable keys() {
+            initKeys();
+            return keys;
+        }
+
+        public Iterable values() {
+            initValues();
+            return values;
+        }
+
+        private void initKeys() {
+            if (keys == null) {
+                HashSet<String> keySet = new HashSet<String>();
+                List<String> keySeq = new ArrayList<>();
+                addKeys(keySet, keySeq, (WrappedHash) this.left);
+                addKeys(keySet, keySeq, (WrappedHash) this.right);
+                size = keySet.size();
+                keys = keySeq;
+            }
+        }
+
+        private static void addKeys(Set<String> set, List<String> keySeq, WrappedHash hash) throws EvaluationException {
+            Iterator<?> it = hash.keys().iterator();
+            while (it.hasNext()) {
+                String s = asString(it.next());
+                if (set.add(s)) {
+                    // The first occurence of the key decides the index;
+                    // this is consisten with stuff like java.util.LinkedHashSet.
+                    keySeq.add(s);
+                }
+            }
+        }
+
+        private void initValues() {
+            if (values == null) {
+                List<Object> seq = new ArrayList<>();
+                for (String key : keys) {
+                    seq.add(get(key));
+                }
+                values = seq;
+            }
+        }
+
+    }
+
+}
+
+
